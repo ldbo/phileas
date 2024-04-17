4fdf38830a9c7ae007bee541ff17946e9815b8f1
Louis Dubois <louis.dubois@ssi.gouv.fr>
2024-04-17 10:55:57 +0200

    iteration: Add iteration leaves size restriction helper function

--- phileas/iteration.py
+++ phileas/iteration.py
@@ -1329,3 +1329,63 @@ class Sequence(IterationLeaf):
         compromise.
         """
         return self.elements[key]  # type: ignore[index]
+
+
+###########
+# Utility #
+###########
+
+
+class RestrictionPolicy(Enum):
+    #: Iteration leaves only keep their first and last values.
+    FIRST_LAST = "FIRST_LAST"
+
+    #: Iteration leaves only keep their two first values.
+    FIRST_SECOND = "FIRST_SECOND"
+
+    #: Iteration leaves keep their first, second and last values.
+    COMBINED = "COMBINED"
+
+
+def restrict_leaves_sizes(
+    tree: IterationTree,
+    policy: RestrictionPolicy = RestrictionPolicy.FIRST_LAST,
+) -> IterationTree:
+    """
+    Restrict the size of the iteration leaves of the tree, depending on the
+    restriction policy. This is useful for troubleshooting, or to verify that
+    the full range of the leaves is supported by *something*.
+    """
+
+    def _restrict(tree: IterationTree, _: ChildPath) -> IterationTree:
+        if not isinstance(tree, IterationLeaf):
+            return tree
+
+        if policy == RestrictionPolicy.FIRST_LAST:
+            if len(tree) <= 2:
+                return tree
+
+            tree_iter = iter(tree)
+            first = next(tree_iter)
+            tree_iter.reverse()
+            tree_iter.reset()
+            last = next(tree_iter)
+            return Sequence([first, last])
+        elif policy == RestrictionPolicy.FIRST_SECOND:
+            if len(tree) <= 2:
+                return tree
+
+            tree_iter = iter(tree)
+            return Sequence([next(tree_iter), next(tree_iter)])
+        else:
+            if len(tree) <= 3:
+                return tree
+
+            tree_iter = iter(tree)
+            first, second = next(tree_iter), next(tree_iter)
+            tree_iter.reverse()
+            tree_iter.reset()
+            last = next(tree_iter)
+            return Sequence([first, second, last])
+
+    return tree.depth_first_modify(_restrict)
